03696eec4eeec9a6e3d8781e265a706c125fc9ee
PCCartem <1.aaaaaa@mail.ru>
2015-10-14 15:47:27 +0600

    Реестр и пул обьектов

--- index.php
+++ index.php
@@ -1,6 +1,7 @@
 <?php 
 require_once("autoloader.php");
-use App\Blog;
+use App\Registry;
+use App\Object;
 ?>
 <!DOCTYPE html>
 <html lang="en">
@@ -9,30 +10,12 @@ use App\Blog;
 	<title>Blog</title>
 </head>
 <body>
-	<div class="menu">
-		<li><a href="/">Главная</a></li>
-		<li><a href="/?category=Общее">Общее</a></li>
-	</div>
-	<h1>Posts</h1>
-		<main>
-			<?php 
-				$blog = Blog::getInstanse();
-				$posts = $blog->viewBlog($_GET['category']);
-
-				if ($posts) {
-					foreach ($posts as $key => $post) {
-					echo '<article>
-						<h2><a href="/post.php?id='.$post['id'].'">'.$post['title'].'</a></h2>
-						<div>'.mb_strimwidth($post['content'], 0, 100).'</div>
-						<div>'.$post['category'].'</div>
-						<div>'.$post['author'].'</div>
-					</article>';
-					}
-				}
-				
-
-			 ?>	
-		</main>
+	<?php 
+		Registry::setValue('name', 'Vasya');
+		echo "Привет ". Registry::getValue('name'). "<br>";
+		Registry::setObject(new Object('test'));
+		echo Registry::getObject('test')->getId();
+	 ?>
 </body>
 </html>
 
